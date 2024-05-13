e4f329f6f8c92ad4ac9f16653b663138e217e5cf
Patrycja-Bien <s-121-19@gumiforest.ae.krakow.pl>
2024-05-13 14:56:27 +0200

    2024-05-13 PB

--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,23 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
-@app.route('/')
-@app.route('/index')
+@app.route('/') #dekorator
 def index():
     return render_template("index.html")
 
-@app.route('/')
-@app.route('/name/<name>')
-def name(name):
-    return f"Hello, {name}!"
-
-@app.route('/extract')
+@app.route('/extract', methods=["POST", "GET"])
 def extract():
+    if request.method=="POST":
+        product_id = request.form.get("product_id")
+        return redirect(url_for("product", product_id = product_id))
     return render_template("extract.html")
 
 @app.route('/products')
 def products():
     return render_template("products.html")
 
-@app.route('/product_id')
-def product():
+@app.route('/product/<product_id>')
+def product(product_id):
     return render_template("product.html", product_id=product_id)
 
 @app.route('/about')
